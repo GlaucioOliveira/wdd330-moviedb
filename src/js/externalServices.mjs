4787f8ac888c33b6d33b7916d8401faf8386fb5a
Gláucio Oliveira <glaucio.dev@gmail.com>
2025-04-07 20:28:33 -0600

    removing temporarily the year from the API

--- src/js/externalServices.mjs
+++ src/js/externalServices.mjs
@@ -23,7 +23,8 @@ export async function getMovieById(id) {
 
 export async function getMovieData(movie, year) {
   const protocol = window.location.hostname === "localhost" ? "http" : "https";
-  const response = await fetch(`${protocol}://www.omdbapi.com/?t=${movie}&y=${year}&apikey=de78baf9`);
+  //const response = await fetch(`${protocol}://www.omdbapi.com/?t=${movie}&y=${year}&apikey=de78baf9`);
+  const response = await fetch(`${protocol}://www.omdbapi.com/?t=${movie}&apikey=de78baf9`);
   const data = await convertToJson(response);
   return data;
 }
